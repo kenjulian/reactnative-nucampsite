43ca008f9cadf27733a03dbf9f72f30006aed145
kenjulian <kenneth.julian@yahoo.com>
2022-05-20 13:49:50 -0400

    Icons, Faves, Comments

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,20 +1,24 @@
 import React, {Component} from 'react';
+import Home from './HomeComponent';
 //import { ThemeConsumer } from 'react-native-elements';
 import Directory from './DirectoryComponent';
 import CampsiteInfo from './CampsiteInfoComponent';
+import About from './AboutComponent';
+import Contact from './ContactComponent';
 import Constants from 'expo-constants';
 import {View, Platform} from 'react-native';
 import {createStackNavigator} from 'react-navigation-stack';
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import { createAppContainer} from 'react-navigation';
 //import ExpoStatusBar from 'expo-status-bar/build/ExpoStatusBar';
 
 //import {CAMPSITES} from './shared/campsites';
-
+//we are creating a stack navigator to be initialized into createAppContainer
 const DirectoryNavigator = createStackNavigator(
     //1 required argument: route configs obj; defines which components are in the stack
     {
         Directory: {screen: Directory},
-        CampsiteInfo: {screen: CampsiteInfo},
+        CampsiteInfo: {screen: CampsiteInfo}
 
     },
     {
@@ -32,7 +36,78 @@ const DirectoryNavigator = createStackNavigator(
     }
 );
 
-const AppNavigator = createAppContainer(DirectoryNavigator);
+//we are creating a stack navigator to be initialized into createAppContainer
+const HomeNavigator = createStackNavigator(
+    {
+        Home: {screen: Home}
+       
+
+    },
+    {
+       
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+
+        }
+    }
+)
+
+const AboutNavigator = createStackNavigator(
+    {
+        About: {screen: About}
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+
+        }
+    }
+)
+
+const ContactNavigator = createStackNavigator(
+    {
+        Contact: {screen: Contact}
+    },
+    {
+        defaultNavigationOptions: {
+            headerStyle: {
+                backgroundColor: '#5637DD'
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                color: '#fff'
+            }
+
+        }
+    }
+)
+
+const MainNavigator = createDrawerNavigator(
+    {
+        Home: {screen: HomeNavigator},
+        Directory: {screen: DirectoryNavigator},
+        About: {screen: AboutNavigator},
+        Contact: {screen: ContactNavigator}
+    },
+    {
+        drawerBackgroundColor: '#CEC8FF'
+    }
+)
+
+//MainNavigator replaced DirectoryNavigator as top level navigator
+const AppNavigator = createAppContainer(MainNavigator);
 
 class Main extends Component {
     //needs constructor to set up state property to hold campsites data
