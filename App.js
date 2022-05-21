07b324a1e0d516be28a33d81783774526e35bb67
kenjulian <kenneth.julian@yahoo.com>
2022-05-21 10:30:34 -0400

    modified aboutcomponent to accomodate to the store

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import Main from './components/MainComponent';
+import {Provider} from 'react-redux';
+import {ConfigureStore} from './redux/configureStore';
+
+
+//creates configures and returns redux store
+const store = ConfigureStore();
 
 export default function App() {
   return (
-    <Main />
+    // pass the store as a prop so MainComponent and its children can access the redux store
+    <Provider store={store}>
+      <Main />
+    </Provider>
   );
 }
 
--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -2,7 +2,15 @@ import React, {Component} from 'react';
 import {Text, ScrollView} from 'react-native';
 import {Card, ListItem} from 'react-native-elements';
 import { FlatList } from 'react-native-gesture-handler';
-import {PARTNERS} from '../shared/partners';
+//import {PARTNERS} from '../shared/partners';
+import {connect} from 'react-redux';
+import {baseUrl} from '../shared/baseUrl';
+
+const mapStateToProps = state => {
+    return {
+        partners: state.partners
+    };
+};
 
 function Mission() {
     return (
@@ -18,12 +26,7 @@ function Mission() {
 }
 
 class About extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            partners: PARTNERS
-        }
-    }
+    
 
     static navigationOptions = {
         title: "About Us"
@@ -34,7 +37,7 @@ class About extends Component {
                 <ListItem
                     title={item.name}
                     subtitle={item.description}
-                    leftAvatar={{source: require('./images/bootstrap-logo.png')}}
+                    leftAvatar={{source: {uri: baseUrl + item.image}}}
                 />
 
             )
@@ -46,7 +49,7 @@ class About extends Component {
 
                     {/* iterates through data prop using renderItem on each data(renders it inside the flatlist) */}
                     <FlatList
-                        data={this.state.partners}
+                        data={this.partners.partners}
                         keyExtractor={item => item.id.toString()}
                         renderItem={renderPartner}
                     />
@@ -59,4 +62,4 @@ class About extends Component {
     }
 }
 
-export default About;
+export default connect(mapStateToProps)(About);
